fbf3dc32768d6c389877b18f7e1cfcea908d93fb
michaela <michaelabix@gmail.com>
2021-12-02 11:10:25 -0500

    remove repeat code day02

--- 02/day02.py
+++ 02/day02.py
@@ -5,42 +5,33 @@ def read_input():
     for f in file_puzzle_input:
         instructions.append(f)
         
-def part1():
+def calculate():
     forward = 0
+    forward2 = 0
     depth = 0
-    for i in instructions:
-        temp = i.split(' ')
-        if temp[0] == 'forward':
-            forward += int(temp[1])
-        elif temp[0] == 'up':
-            depth -= int(temp[1])
-        elif temp[0] == 'down':
-            depth += int(temp[1])
-        else:
-            print("invalid input")
-    return forward * depth
-
-def part2():
-    forward = 0
-    depth = 0
+    depth2 = 0
     aim = 0
     for i in instructions:
         temp = i.split(' ')
         if temp[0] == 'forward':
             forward += int(temp[1])
-            depth += aim * int(temp[1])
+            forward2 += int(temp[1])
+            depth2 += aim * int(temp[1])
         elif temp[0] == 'up':
+            depth -= int(temp[1])
             aim -= int(temp[1])
         elif temp[0] == 'down':
+            depth += int(temp[1])
             aim += int(temp[1])
         else:
             print("invalid input")
-    return depth * forward
+    return forward * depth, depth2 * forward2
 
 def main():
     read_input()
-    print("the answer for part 1 is: " + str(part1()))
-    print("the answer for part 2 is: " + str(part2()))
+    answer = calculate()
+    print("the answer for part 1 is: " + str(answer[0]))
+    print("the answer for part 2 is: " + str(answer[1]))
 
 if __name__ == "__main__":
      main()
