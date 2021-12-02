cc8d4a3e16d66c98b86e05e46904bc0046a64236
michaela <michaelabix@gmail.com>
2021-12-02 11:17:18 -0500

    refactor day02 more

--- 02/day02.py
+++ 02/day02.py
@@ -1,13 +1,12 @@
-instructions = []
-
 def read_input():
     file_puzzle_input = open("puzzle_input", 'r').read().splitlines()
+    instructions = []
     for f in file_puzzle_input:
         instructions.append(f)
+    return instructions
         
-def calculate():
+def calculate(instructions):
     forward = 0
-    forward2 = 0
     depth = 0
     depth2 = 0
     aim = 0
@@ -15,7 +14,6 @@ def calculate():
         temp = i.split(' ')
         if temp[0] == 'forward':
             forward += int(temp[1])
-            forward2 += int(temp[1])
             depth2 += aim * int(temp[1])
         elif temp[0] == 'up':
             depth -= int(temp[1])
@@ -25,13 +23,12 @@ def calculate():
             aim += int(temp[1])
         else:
             print("invalid input")
-    return forward * depth, depth2 * forward2
+    return forward * depth, depth2 * forward
 
 def main():
-    read_input()
-    answer = calculate()
-    print("the answer for part 1 is: " + str(answer[0]))
-    print("the answer for part 2 is: " + str(answer[1]))
+    instructions = read_input()
+    answer = calculate(instructions)
+    print("the answer for part 1 is: " + str(answer[0]) + "\n" + "the answer for part 2 is: " + str(answer[1]))
 
 if __name__ == "__main__":
      main()
