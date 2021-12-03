89d13d9da61b1712ec34784f8bb23fd7c06e157a
michaela <michaelabix@gmail.com>
2021-12-03 08:09:20 -0500

    add day03, refactor later

---
+++ 03/day03.py
@@ -0,0 +1,78 @@
+def read_input():
+	file_puzzle_input = open("puzzle_input", 'r').read().splitlines()
+	return file_puzzle_input
+
+def part1(input):
+	x = 0
+	length = len(input[0])
+	gamma = ""
+	epsilon = ""
+	while x < length:
+		temp = []
+		for i in input:
+			temp.append(i[x])
+		positive = temp.count("1")
+		zero = temp.count("0")
+		if positive > zero:
+			gamma += "1"
+			epsilon += "0"
+		elif zero > positive:
+			gamma += "0"
+			epsilon += "1"
+		else:
+			print("something went wrong or positives and zero cannot be equal")
+		x += 1
+	gamma_num = int(gamma, 2)
+	epsilon_num = int(epsilon, 2)
+	return gamma_num * epsilon_num
+
+def oxygen(values):
+	input = values.copy()
+	x = 0
+	length = len(input[0])
+	while (x < length) and (len(input) > 1):
+		temp = []
+		keep = "0"
+		for i in input:
+			temp.append(i[x])
+		positive = temp.count("1")
+		zero = temp.count("0")
+		if positive > zero or positive == zero:
+			keep = "1"
+		for index, i in reversed(list(enumerate(input))):
+			if i[x] != keep:
+				input.pop(index)
+		x += 1
+	return int(input[0], 2)
+	
+def co2(values):
+	input = values.copy()
+	x = 0
+	length = len(input[0])
+	while (x < length) and (len(input) > 1):
+		temp = []
+		keep = "0"
+		for i in input:
+			temp.append(i[x])
+		positive = temp.count("1")
+		zero = temp.count("0")
+		if positive < zero:
+			keep = "1"
+		for index, i in reversed(list(enumerate(input))):
+			if i[x] != keep:
+				input.pop(index)
+		x += 1
+	return int(input[0], 2)
+
+def part2(input):
+	o2 = oxygen(input)
+	c = co2(input)
+	return c * o2
+	
+def main():
+	input = read_input()
+	print("the answer to part 1 is: " + str(part1(input)))
+	print("the answer to part 2 is: " + str(part2(input)))
+
+if __name__ == "__main__":
+	main()
