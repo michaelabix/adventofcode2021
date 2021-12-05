b06a6056d420cbaccf8aff9a8e2741396613286a
michaela <michaelabix@gmail.com>
2021-12-04 22:14:23 -0500

    fix backwards index for vertical checking & finish part 2

--- 04/day04.py
+++ 04/day04.py
@@ -34,17 +34,17 @@ class bingo_card:
 		while i < y:
 			count_v = 0
 			while j < x:
-				if self.track_card[i][j] == -1:
+				if self.track_card[j][i] == -1:
 					count_v += 1
 					if count_v == y:
 						self.set_score(num)
 						c_bingo = 1
+						count_v = 0
 						break
 					else:
 						j += 1
 						continue
 				else:
-					count_v = 0
 					break
 			i += 1	
 		return c_bingo
@@ -88,7 +88,7 @@ def parse_bingo():
 		cards.append(card)
 	return nums, cards
 
-def part1(nums, bingo):
+def play(nums, bingo):
 	#play game
 	for b in bingo:
 		for i, n in enumerate(nums):
@@ -96,17 +96,24 @@ def part1(nums, bingo):
 			if not b.get_score():
 				b.check_num(int(n), turns)
 	lowest = 0
+	highest = 0
 	previous = bingo[0].get_score()
 	for i, b in enumerate(bingo):
 		if b.get_turns() < previous:
 			lowest = i
 			previous = b.get_turns()
-	return bingo[lowest].get_score()
+
+	for i, b in enumerate(bingo):
+		if b.get_turns() > previous:
+			highest = i
+			previous = b.get_turns()
+	return bingo[lowest].get_score(), bingo[highest].get_score()
 
 def main():
 	nums, bingo = parse_bingo()
-	print("the answer to part 1 is: " + str(part1(nums, bingo)))
-#	print("the answer to part 2 is: " + str(part2(input)))
+	answers = play(nums, bingo)
+	print("the answer to part 1 is: " + str(answers[0]))
+	print("the answer to part 2 is: " + str(answers[1]))
 
 if __name__ == "__main__":
 	main()
